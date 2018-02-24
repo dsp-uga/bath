58475e67cb1d0b53852efc7e89f97d72bea68a0d
raun <rd31879@uga.edu>
2018-02-24 16:30:30 -0500

    Updated to generate the y_test as well

--- preprocess.py
+++ preprocess.py
@@ -2,9 +2,13 @@ import numpy as np
 from PIL import Image
 import glob
 from operator import add
-path="D:/DSP_003/"
+
+path="/home/rdey/DSP_p3/DSP_003/"
+#path="D:\DSP_003/"
+
 X_train=[]
 for i in range(0,19):
+	print(i)
 	counter=0
 	temp=[]
 	var_to_add=np.zeros((512,512), dtype=np.float32).tolist()
@@ -21,6 +25,25 @@ np.save("X_train",np.array(X_train))
 
 
 ###############################################################################################
+import json
+import glob
+import numpy as np
+import cv2
+y_test=[]
+for i in range(0,19):
+	print(i)
+
+	mask=np.zeros((512,512), dtype=np.float32)
+	data = json.load(open(path+str(i)+"/regions/regions.json"))
+	for j in range(0,len(data)):
+		#print(data[j]['coordinates'])
+		for k in range(0,len(data[j]['coordinates'])):
+
+			mask[(data[j]['coordinates'][k][0]),(data[j]['coordinates'][k][1])]=1
+	y_test.append(mask)
+	cv2.imwrite("mask"+str(i)+".png",mask*255)
+
+
 
 
 
