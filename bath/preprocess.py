a61d24dfd5121a6903a7149368815ca8d289188e
Zach Jones <zachydj@gmail.com>
2018-03-01 22:03:37 -0500

    Refactor data loader to load sets by name

--- bath/preprocess.py
+++ bath/preprocess.py
@@ -11,37 +11,44 @@ import json
 from bath.Dataset import Dataset
 
 
-def load(name, location, greyscale=True):
+def name_to_directory(name):
+    return f'neurofinder.{name}'
+
+
+def load(name, base_dir, greyscale=True):
     """
     Loads a single example from disk and converts it to a bath.Dataset object.
     This function assumes a directory structure similar to that of the codeneuro datasets.  That is, the input directory
-    (location) should contain an "images" subdirectory and optionally a "regions" subdirectory.
+    (base_dir) should contain an "neurofinder.<name>" subdirectory which contains an "images" subdirectory and an
+    optional "regions" subdirectory.
     If a regions subdirectory cannot be found in the given location, then regions will not be loaded.
 
-    :param name: the logical name of the dataset
-    :param location: the directory where the data are located - should have a subdirectory "images"
+    Example: load('00.00', './data') will load the dataset contained in the directory './data/neurofinder.00.00/images'
+    and, if found, the regions contained in the directory './data/neurofinder.00.00/regions'
+
+    :param name: the logical name of the dataset (will be converted to the directory name "neurofinder.<name>")
+    :param base_dir: the directory where the the dataset folder is located
     :param greyscale: load the images as greyscale.  Defaults to true
     :return: bath.Dataset representing this sequence of images
     """
+    data_directory = os.path.join(base_dir, name_to_directory(name))
     # make sure the input directory exists:
-    assert(os.path.exists(os.path.join(location, "images")))
+    assert(os.path.exists(os.path.join(data_directory, "images")))
 
     # load images into a np array
-    file_pattern = os.path.join(location, "images", "*.tiff")
+    file_pattern = os.path.join(data_directory, "images", "*.tiff")
     files = sorted(glob(file_pattern))
     images = np.array([io.imread(f, as_grey=greyscale) for f in files])
 
     dataset = Dataset(name, images)
 
     # load regions if available
-    has_regions = os.path.exists(os.path.join(location, 'regions'))
+    has_regions = os.path.exists(os.path.join(data_directory, 'regions'))
     if has_regions:
-        regions_file = os.path.join(location, 'regions', 'regions.json')
+        regions_file = os.path.join(data_directory, 'regions', 'regions.json')
         with open(regions_file) as file:
             regions = json.load(file)
-            # contents = file.read()
 
-        # regions = json.loads(contents)
         dataset.set_regions(regions)
 
     return dataset
